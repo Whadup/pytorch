086987e8586eab2c8c82a0aae8635adb4759f50c
Laith Sakka <lsakka@meta.com>
2025-03-03 23:54:34 -0800

    add gaurd else true, guard else false and gaurd_size_oblivious in decompositions.py
    ghstack-source-id: 424fdbfa783aaf6767415015c4d370f608422c57
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/148430

--- torch/fx/experimental/symbolic_shapes.py
+++ torch/fx/experimental/symbolic_shapes.py
@@ -1149,6 +1149,31 @@ def compute_unbacked_bindings(
     return symbol_to_path
 
 
+# This is used for size oblivious reasoning to avoid 0/1 specializations.
+def guard_else_false(a: BoolLikeType) -> bool:
+    """
+    try to gaurd a, if data dependent error encountered just return false.
+    """
+    if isinstance(a, SymBool):
+        try:
+            guard_bool(a)
+        except GuardOnDataDependentSymNode:
+            return False
+    return bool(a)
+
+
+def guard_else_true(a: BoolLikeType) -> bool:
+    """
+    try to gaurd a, if data dependent error encountered just return true.
+    """
+    if isinstance(a, SymBool):
+        try:
+            guard_bool(a)
+        except GuardOnDataDependentSymNode:
+            return True
+    return bool(a)
+
+
 def definitely_true(a: BoolLikeType) -> bool:
     """
     Returns True only if we can tell that a is True, possibly introducing
